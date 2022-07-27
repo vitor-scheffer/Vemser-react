05196c3cc8a9f26af7dcd3b62fd14bc56153c8f6
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-07-27 00:18:44 -0300

    Adicionando seção endereço com api viaCep.

--- tarefa-login/src/pages/Endereco.js
+++ tarefa-login/src/pages/Endereco.js
@@ -1,8 +1,17 @@
 import { useFormik } from 'formik'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { AuthContext } from '../context/AuthContext'
+import apiCEP from '../Services/apiCEP'
+
+
 
 const Endereco = () => {
+  const [rua, setRua] = useState()
+  const [bairro, setBairro] = useState()
+  const [cidade, setCidade] = useState()
+  const [estado, setEstado] = useState()
+
+
   const formik = useFormik({
     initialValues: {
       tipo: '',
@@ -15,56 +24,97 @@ const Endereco = () => {
       pais: ''
     },
     onSubmit: values => {
-
+      console.log(values)
     }
   })
 
+  const procuraCep = async () => {
+    const cep = formik.values.cep.replaceAll('-', '')
+    try {
+      const { data } = await apiCEP.get(`/${parseInt(cep)}/json/`)
+      setRua(data.logradouro);
+      setBairro(data.bairro);
+      setCidade(data.localidade);
+      setEstado(data.uf);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const zeraCampos = () => {
+    formik.values.logradouro = ''
+  }
+
+  // procuraCep(formik.values.cep)
+  
   
   return (
     <>
     <h1>Busque seu endereço</h1>
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="login">cep</label>
+    <form style={{display: 'flex', flexDirection: 'column', width: '350px', gap:'5px'}}onSubmit={formik.handleSubmit}>
+      <label htmlFor="cep">cep</label>
       <input type="text"
-      id="login"
-      name="login"
+      id="cep"
+      name="cep"
       onChange={formik.handleChange}
-      value={formik.values.login}
+      onBlur={procuraCep}
+      onKeyDown={zeraCampos} 
+      value={formik.values.cep}
       />
-      <label htmlFor="senha">Rua</label>
-      <input type="password"
-      id="senha"
-      name="senha"
-      onChange={formik.handleChange}
-      value={formik.values.senha}
+      <label htmlFor="rua">Tipo</label>
+      <select name="tipo" id="tipo" onBlur={formik.handleChange}>
+        <option value=''></option>
+        <option value='COMERCIAL'>Comercial</option>
+        <option value='RESIDENCIAL'>Residencial</option>
+      </select>
+      <label htmlFor="rua">Rua</label>
+      <input type="text"
+      id="rua"
+      name="rua"
+      onChange={formik.values.logradouro = rua}
+      value={rua}
       />
-      <label htmlFor="senha">Complemento</label>
-      <input type="password"
-      id="senha"
-      name="senha"
+      <label htmlFor="rua">Número</label>
+      <input type="text"
+      id="numero"
+      name="numero"
       onChange={formik.handleChange}
-      value={formik.values.senha}
+      value={formik.values.numero}
       />
-      <label htmlFor="senha">Bairro</label>
-      <input type="password"
-      id="senha"
-      name="senha"
-      onChange={formik.handleChange}
-      value={formik.values.senha}
+      <label htmlFor="bairro">Bairro</label>
+      <input type="text"
+      id="bairro"
+      name="bairro"
+      onChange={formik.values.bairro = bairro}
+      value={bairro}
       />
-      <label htmlFor="senha">Cidade</label>
-      <input type="password"
-      id="senha"
-      name="senha"
-      onChange={formik.handleChange}
-      value={formik.values.senha}
+      <label htmlFor="cidade">Cidade</label>
+      <input type="text"
+      id="cidade"
+      name="cidade"
+      onChange={formik.values.cidade = cidade}
+      value={cidade}
       />
-      <label htmlFor="senha">Estado</label>
-      <input type="password"
-      id="senha"
-      name="senha"
+      <label htmlFor="estado">Estado</label>
+      <input type="text"
+      id="estado"
+      name="estado"
+      onChange={formik.values.estado = estado}
+      value={estado}
+      />
+      <label htmlFor="pais">País</label>
+      <input type="text"
+      id="pais"
+      name="pais"
       onChange={formik.handleChange}
-      value={formik.values.senha}
+      value={formik.values.pais}
+      />
+      <label htmlFor="complemento">Complemento</label>
+      <input type="text"
+      id="complemento"
+      name="complemento"
+      onChange={formik.handleChange}
+      value={formik.values.complemento}
       />
       
       <button type="submit">Cadastrar</button>
